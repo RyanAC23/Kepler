1478aca2c37f8260b07678c6f48568aaed86b8a1
= <ryanacorbin@gmail.com>
2022-03-08 22:56:28 -0800

    Fixes to conf.py

--- Docs/conf.py
+++ Docs/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+ # import os
+ # import sys
+ # sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
@@ -31,20 +31,22 @@ release = "0"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "myst_parser",
-    # "myst_nb",
-    #    "sphinx.ext.autodoc",
-    #    "sphinx.ext.doctest",
-    #    "sphinx.ext.intersphinx",
-    #    "sphinx.ext.coverage",
-    #    "sphinx.ext.mathjax",
-    #    "sphinx.ext.ifconfig",
-    #    "sphinx.ext.viewcode",
-    #    "sphinx.ext.napoleon",
-    #    "sphinx.ext.todo",
-    #    "sphinxcontrib.zopeext.autointerface",
-    #    "matplotlib.sphinxext.plot_directive",
-    #    "sphinxcontrib.bibtex",
+    "myst_nb",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.coverage",
+    "sphinx.ext.doctest",
+    "sphinx.ext.ifconfig",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+#    "sphinxcontrib.bibtex",
+    "sphinxcontrib.zopeext.autointerface",
+    "matplotlib.sphinxext.plot_directive",
+#    "sphinx_comments",  # Hypothes.is comments and annotations
+    "sphinx_panels",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -53,14 +55,59 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
 
+source_suffix = {
+    ".rst": "restructuredtext",  # Make sure this is first!
+    ".myst": "myst-nb",
+    ".md": "myst-nb",
+}
+
+## https://myst-parser.readthedocs.io/en/latest/using/syntax-optional.html
+# https://myst-parser.readthedocs.io/en/latest/syntax/optional.html#substitutions-with-jinja2
+myst_enable_extensions = [
+    "amsmath",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "html_admonition",
+    "html_image",
+    # "linkify",
+    "replacements",
+    "smartquotes",
+    "substitution",
+    # "tasklist",
+]
+
+# autosummary settings
+autosummary_generate = True
+autosummary_generate_overwrite = False
+autosummary_imported_members = False
+add_module_names = False
+
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# Cache notebook output to speed generation.
+# https://myst-nb.readthedocs.io/en/latest/use/execute.html
+jupyter_execute_notebooks = "cache"
+execution_allow_errors = True
+execution_timeout = 300
+nbsphinx_timeout = 300  # Time in seconds; use -1 for no timeout
 
 # -- Options for HTML output -------------------------------------------------
 
+
+# Override version number in title... not relevant for docs.
+html_title = project
+
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
+
+templates_path = ["_templates"]
 html_theme = "traditional"
 
 # Add any paths that contain custom static files (such as style sheets) here,
